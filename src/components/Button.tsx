e0892572480afdc46472bda67c3a89e7829e0840
Andrey Deykun <rrreydan@gmail.com>
2023-02-20 21:05:26 +0300

    Make a full homepage

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,18 @@
+import Icon from '@mdi/react';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
 interface ButtonProps {
   title: string;
   href: string;
+  icon?: string;
 }
 
-const Button: FC<ButtonProps> = ({ title, href }) => {
+const Button: FC<ButtonProps> = ({ href, title, icon }) => {
   return (
-    <button className="bg-secondary hover:bg-white text-white hover:text-secondary px-5 py-2 rounded-[30px] max-w-[200px] font-medium xl:text-[24px] lg:text-[19px] text-[14px] shadow-[0_0_20px_5px] lg:shadow-[0_0_40px_5px] lg:shadow-secondary/30 shadow-secondary/30 hover:shadow-secondary/40 transition-all">
+    <button className="flex items-center justify-center gap-2 h-[40px] w-[150px] lg:h-[60px] lg:w-[200px] outline outline-white hover:bg-white text-white hover:text-secondary px-5 py-2 rounded-[30px] max-w-[200px] font-medium xl:text-[24px] lg:text-[19px] text-[14px] shadow-[0_0_20px_5px] lg:shadow-[0_0_40px_5px] lg:shadow-secondary/30 shadow-secondary/30 hover:shadow-secondary/40 transition-all">
+      {icon && <Icon className="transition-colors" path={icon} size={2} />}
+
       <Link to={href}>{title}</Link>
     </button>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,10 @@ import { IParallax, Parallax, ParallaxLayer } from '@react-spring/parallax';
 import { FC, useRef, useState } from 'react';
 import AboutMe from '../components/AboutMe';
 import Hero from '../components/Hero';
+import MyLinks from '../components/MyLinks';
 import Navbar from '../components/Navbar';
 import Skills from '../components/Skills';
+import styles from '../style';
 
 const Home: FC = () => {
   const parallax = useRef<IParallax>(null!);
@@ -19,7 +21,7 @@ const Home: FC = () => {
     <Parallax
       ref={parallax}
       id="parallax"
-      pages={2}
+      pages={2.3}
       style={{
         background: '#0d0d0d',
         overflow: `${!sidebarToggle ? 'hidden' : 'hidden scroll'}`
@@ -47,14 +49,14 @@ const Home: FC = () => {
       </ParallaxLayer>
 
       <ParallaxLayer
-        className="flex items-center justify-center"
+        className="flex items-center justify-center rounded-[40px] shadow-[0_0_20px_5px] lg:shadow-[0_0_30px_5px] lg:shadow-secondary/10 shadow-secondary/10"
         offset={0.999}
         speed={1.7}
         factor={1.3}
         style={{
           background: '#100f26'
         }}
-        onClick={() => parallax.current.scrollTo(0.99)}
+        onClick={() => parallax.current.scrollTo(0.94)}
       >
         <AboutMe />
       </ParallaxLayer>
@@ -68,9 +70,28 @@ const Home: FC = () => {
           background: '#0d0d0d',
           zIndex: '-1'
         }}
-        onClick={() => parallax.current.scrollTo(0)}
+        onClick={() => parallax.current.scrollTo(1.3)}
       >
         <Skills />
+      </ParallaxLayer>
+
+      <ParallaxLayer
+        className="flex flex-col items-center m-h-[350px] justify-start rounded-t-[40px] shadow-[0_0_20px_5px] lg:shadow-[0_0_30px_5px] lg:shadow-secondary/10 shadow-secondary/10"
+        offset={1.85}
+        speed={1.2}
+        style={{
+          background: '#100f26',
+          maxWidth: '1000px',
+          marginLeft: 'auto',
+          marginRight: 'auto'
+        }}
+      >
+        <MyLinks />
+        <h1
+          className={`${styles.paddingX} ${styles.flexCenter} w-full text-secondary mt-[115px] font-inter font-semibold xl:text-[72px] lg:text-[62px] text-[40px]`}
+        >
+          There's nothing else here :(
+        </h1>
       </ParallaxLayer>
     </Parallax>
   );
